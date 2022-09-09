304112204c8484567eedc18f0baf58aa65c470aa
lariosd <davilarios450@gmail.com>
2022-09-09 15:19:50 -0500

    add: styles and componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { HeadersComponent } from './shared/headers/headers.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NgModule } from '@angular/core';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SidebarsComponent } from './shared/sidebars/sidebars.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotpagefoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarsComponent,
+    HeadersComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
